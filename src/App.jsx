c804a5ac487157648a44aa51419dd512c52b68ae
unknown <misaelucas@gmail.com>
2022-02-25 01:42:42 -0300

    reseting readme.md

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <p>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>{" "}
+    </p>
+  );
+}
+
+export default App;
